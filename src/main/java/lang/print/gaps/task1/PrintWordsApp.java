95d6f49e1f0a5557e09a926fc52364d1c4e78f95
LChkheidze <levanchx@gmail.com>
2023-05-08 23:09:14 +0400

    Fixed and updated

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("Java");
+        System.out.println("java");
         System.out.println("is");
         System.out.println("smth");
     }
